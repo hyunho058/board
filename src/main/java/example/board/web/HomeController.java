06bfc6ebf8e661d97b72d04a51cd7be5fc5ac3b1
hyunho058 <hyunho058@naver.com>
2022-01-27 23:44:10 +0900

    쿠키를 이용한 로그인 기능 추가

--- src/main/java/example/board/web/HomeController.java
+++ src/main/java/example/board/web/HomeController.java
@@ -1,15 +1,54 @@
 package example.board.web;
 
+import example.board.domain.member.Member;
+import example.board.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
+    private final MemberRepository memberRepository;
+//    @GetMapping("/")
+//    public String home(){
+//        return "home";
+//    }
+
     @GetMapping("/")
-    public String home(){
-        return "home";
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+        if (memberId == null) {
+            return "home";
+        }
+
+        //login
+        Member loginMeber = memberRepository.findById(memberId);
+        if (loginMeber == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMeber);
+        return "loginHome";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response){
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
--- src/main/java/example/board/web/login/LoginController.java
+++ src/main/java/example/board/web/login/LoginController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Controller
@@ -22,7 +24,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginForm loginForm, BindingResult bindingResult){
+    public String login(@Valid @ModelAttribute LoginForm loginForm, BindingResult bindingResult, HttpServletResponse response){
         if (bindingResult.hasErrors()){
             return "login/loginForm";
         }
@@ -33,6 +35,12 @@ public class LoginController {
             bindingResult.reject("LoginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
             return "login/loginForm";
         }
+
+        //로그인 성고 처리(cookie)
+        //쿠키에 시간정보를 주지 않으면 새션 쿠키(브라우저 종료시 모두 종료) =>(참고 - 쿠키 말료일을 지정한것은 영속쿠키라한다)
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
+
 
         return "redirect:/";
     }
